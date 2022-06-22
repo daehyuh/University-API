8c64669850a223de373bdbf1d82ade033c1a6bd3
HanSeokSeo <56254170+DestroySera@users.noreply.github.com>
2022-06-23 01:46:09 +0900

    Edit Database Connection

--- 전형적인 닷컴/Back-end/src/dao/DBConn.java
+++ 전형적인 닷컴/Back-end/src/dao/DBConn.java
@@ -12,9 +12,9 @@ public class DBConn {
 	
 	public DBConn() {
 		String driver = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "hanseok";
-		String password = "1234";
+		String url = "jdbc:oracle:thin:@255.255.255.255:1521:xe"; // 데이터베이스 외부접속 255.255.255.255 임시 아이피
+		String user = "api"; // 데이터베이스 아이디
+		String password = "apiproject"; // 데이터베이스 비밀번호
 		
 		try{
 			Class.forName(driver);
