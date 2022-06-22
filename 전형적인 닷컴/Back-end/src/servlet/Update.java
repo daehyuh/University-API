cb0797c4effbdae443ec0299f6b42e208f552435
HanSeokSeo <56254170+DestroySera@users.noreply.github.com>
2022-06-23 01:34:58 +0900

    BackEnd Java file Commit

---
+++ 전형적인 닷컴/Back-end/src/servlet/Update.java
@@ -0,0 +1,69 @@
+	package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DAO;
+
+/**
+ * Servlet implementation class Update
+ */
+@WebServlet("/Update")
+public class Update extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Update() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String id = request.getParameter("um_id");
+		String pw = request.getParameter("um_password");
+		String name = request.getParameter("um_name");
+		
+		try{
+			DAO.MemberUpdate(id, pw, name);
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('수정완료 성공.');");
+			out.println("location.href='/'"); // 로케이션 처리
+			out.println("</script>");
+			out.close();
+		} catch(Exception e){
+			e.printStackTrace();
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('회원정보 수정중 에러가 발생했습니다.');");
+			out.println("window.history.back()");
+			out.println("</script>");
+			out.close();
+		}
+	}
+
+}
