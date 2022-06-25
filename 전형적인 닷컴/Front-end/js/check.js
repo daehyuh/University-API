2a19dac3dc96e3c637e30ec99d7cc6035348df25
HG04 <74530048+hg04hg@users.noreply.github.com>
2022-06-25 20:19:31 +0900

    Update check.js

--- 전형적인 닷컴/Front-end/js/check.js
+++ 전형적인 닷컴/Front-end/js/check.js
@@ -2,19 +2,20 @@ function fn_login(){
     const form = document.loginForm;
 
     const um_id = form.um_id.value;
-    const um_password = form.um_password.value;
+    const um_pwd = form.um_pwd.value;
 
-    if(um_id == "" || um_id == null || um_password == "" || um_password == null){
+    if(um_id == "" || um_id == null || um_pwd == "" || um_pwd == null){
         document.getElementById("warning").style.display="block";
     } else{
         document.getElementById("warning").style.display="none";
+        form.submit();
     }
 }
 
 
 function btn_cancel(obj){
     if(confirm(obj + "을 취소하고 메인화면으로 돌아가시겠습니까?")){
-        location.replace("index.html");
+        location.replace("index.jsp");
     }
 }
 
@@ -23,19 +24,19 @@ function btn_save(){
     const form = document.joinForm;
 
     const um_id = form.um_id.value;
-    const um_password = form.um_password.value;
+    const um_pwd = form.um_pwd.value;
     const pwcheck = form.um_password_check.value;
     const um_name = form.um_name.value;
 
-    if(um_id.length < 5 || um_password.length < 8 || pwcheck != um_password || um_name == ""){
+    if(um_id.length < 5 || um_pwd.length < 8 || pwcheck != um_pwd || um_name == ""){
 
         if(um_id.length < 5 ){
             document.getElementById("id_warning").style.display="block"; 
         } 
-        if(um_password.length < 8 ){
+        if(um_pwd.length < 8 ){
             document.getElementById("pw_warning").style.display="block";
         } 
-        if(pwcheck != um_password){
+        if(pwcheck != um_pwd){
             document.getElementById("pwcheck_warning").style.display="block";
         }
         if(um_name == ""){
@@ -46,7 +47,7 @@ function btn_save(){
         alert("정상적으로 회원가입이 완료되었습니다.")
         form.submit();
 
-        //um_id(아이디), um_password(비밀번호), um_name(닉네임) submit
+        //um_id(아이디), um_pwd(비밀번호), um_name(닉네임) submit
     }
 
 }
@@ -56,17 +57,17 @@ function btn_update(){
 	
     const form = document.updateForm;
     
-    const update_pw = form.update_pw.value;
+    const update_pwd = form.update_pwd.value;
     const update_pwchk = form.update_pwchk.value;
     const update_name = form.update_name.value;
 
 
-    if(update_pw < 8 || update_pwchk != update_pw || update_name == ""){
+    if(update_pw < 8 || update_pwchk != update_pwd || update_name == ""){
 
-        if(update_pw.length < 8 ){
+        if(update_pwd.length < 8 ){
             document.getElementById("pw_warning").style.display="block";
         } 
-        if(update_pwchk != update_pw){
+        if(update_pwchk != update_pwd){
             document.getElementById("pwcheck_warning").style.display="block";
         }
         if(update_name == ""){
@@ -108,9 +109,9 @@ function check(obj, num, keyCode){
 
 function pwcheck(obj, pw){
 
- 	const password = document.getElementById(pw).value;
+ 	const pwd = document.getElementById(pw).value;
 
-    if(obj.value != password){
+    if(obj.value != pwd){
         document.getElementById("pwcheck_warning").style.display="block";
     } else{
         document.getElementById("pwcheck_warning").style.display="none";
